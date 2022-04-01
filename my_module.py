5dd148a3e3ce5a3b2c7b60e6b99a3da2e3b20a30
Kinga Głąbińska <kingagla@gmail.com>
2022-04-01 19:34:30 +0200

    rgfvsgvesdsd

--- my_module.py
+++ my_module.py
@@ -1,23 +1,9 @@
-def sum(a, b):
-    return a + b
-
+def sum(a,b):
+  return a + b
 
 def multiply(a, b):
-    return a * b
+  return a * b
 
 
-# TODO: example with args and kwargs
-
-def main(a, b):
-    print(f'Sum: 3 + 3 = {sum(a, b)}')
-    print(f'multiplication: 3 + 3 = {multiply(a, b)}')
-
-
-if __name__ == '__main__':
-    try:
-        a, b = 'a', 3
-        main(a, b)
-    except TypeError as err:
-        print(err)
-        a, b = 3, 3
-        main(a, b)
+print(f'Sum: 3 + 3 = {sum(3,3)}')
+print(f'multiplication: 3 + 3 = {multiply(3,3)}')

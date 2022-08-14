64348c757e03c72e9a64fa3e23ad2912bf126373
Kinga Głąbińska <60899123+kingagla@users.noreply.github.com>
2022-08-14 17:51:33 +0200

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -9,13 +9,12 @@ def multiply(a, b):
 # TODO: example with args and kwargs
 
 def main(a, b):
-    print(f'Sum: 3 + 3 = {sum(a, b)}')
     print(f'multiplication: 3 + 3 = {multiply(a, b)}')
-
+    print(f'Sum: 3 + 3 = {sum(a, b)}')
 
 if __name__ == '__main__':
     try:
-        a, b = 'a', 3
+        a, b = '3', 3
         main(a, b)
     except TypeError as err:
         print(err)
